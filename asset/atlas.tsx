fc73a39f00bafb92b7192cc7700e01d0da573c0d
ftiguidou <florent.tiguidou@gmail.com>
2021-08-27 17:34:53 +0200

    Atlas added and first design for level

---
+++ asset/atlas.tsx
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="js13k2021" tilewidth="23" tileheight="16" tilecount="28" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="BG/bg_tile.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="BG/nostars.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="BG/stars1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="BG/stars2.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="BG/window_corner.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="BG/window_side.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="Characters/enenemy_human_butcher.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2.78261" y="0.0434783" width="11.1304" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <image width="18" height="16" source="Characters/ennemy_butcher.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.95652" y="-0.0434783" width="11.9565" height="16.087"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <image width="18" height="16" source="Characters/ennemy_shield_butcher.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2.86957" y="0.0434783" width="15.0435" height="15.9565"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <image width="16" height="16" source="Characters/pnj_cow.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0434783" y="3.82609" width="15.9565" height="12.087"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <image width="16" height="16" source="déco/aprons.png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="16" source="déco/blood1.png"/>
+ </tile>
+ <tile id="12">
+  <image width="16" height="16" source="déco/blood2.png"/>
+ </tile>
+ <tile id="13">
+  <image width="16" height="16" source="déco/knifes.png"/>
+ </tile>
+ <tile id="14">
+  <image width="16" height="16" source="déco/steak.png"/>
+ </tile>
+ <tile id="15">
+  <image width="16" height="16" source="Tiles/cracked_intern_floor_tile.png"/>
+ </tile>
+ <tile id="16">
+  <image width="16" height="16" source="Tiles/cracked_wall.png"/>
+ </tile>
+ <tile id="17">
+  <image width="16" height="16" source="Tiles/floor_tile.png"/>
+ </tile>
+ <tile id="18">
+  <image width="16" height="16" source="Tiles/intern_floor_tile.png"/>
+ </tile>
+ <tile id="19">
+  <image width="16" height="16" source="Tiles/spikes.png"/>
+ </tile>
+ <tile id="21">
+  <image width="16" height="16" source="Tiles/moving_platform.png"/>
+ </tile>
+ <tile id="22">
+  <image width="16" height="16" source="Tiles/cracked_block.png"/>
+ </tile>
+ <tile id="23">
+  <image width="18" height="16" source="Characters/charac_cowboy_gun.png"/>
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="1" y="-0.0869565" width="12.0435" height="16.087"/>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <image width="18" height="16" source="Characters/charac_cowboy_walkframe_gun.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="1.08696" y="0.782609" width="12.9565" height="15.2609"/>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <image width="8" height="6" source="bullet.png"/>
+ </tile>
+ <tile id="26">
+  <image width="17" height="16" source="Characters/charac_cow.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0869565" y="3.73913" width="14.2174" height="12.3043"/>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <image width="23" height="16" source="Characters/charac_cow_dashframe.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.91304" y="5.08696" width="14.6087" height="10.8696"/>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <image width="17" height="16" source="Characters/charac_cow_walkframe.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0434783" y="3.95652" width="14.087" height="12.087"/>
+  </objectgroup>
+ </tile>
+</tileset>
