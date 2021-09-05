d0be92c22fd700e597f5523e73c4160c28a79c6d
ftiguidou <florent.tiguidou@gmail.com>
2021-09-05 18:32:46 +0200

    atlas fixed

--- asset/atlas.tsx
+++ asset/atlas.tsx
@@ -2,82 +2,80 @@
 <tileset version="1.5" tiledversion="1.7.2" name="js13k2021" tilewidth="32" tileheight="16" tilecount="25" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="16" height="16" source="BG/bg_tile.png"/>
+  <image width="16" height="16" source="tiles/bg_tile.png"/>
  </tile>
  <tile id="1">
-  <image width="16" height="16" source="BG/nostars.png"/>
+  <image width="16" height="16" source="tiles/nostars.png"/>
  </tile>
  <tile id="2">
-  <image width="16" height="16" source="BG/stars1.png"/>
+  <image width="16" height="16" source="tiles/stars1.png"/>
  </tile>
  <tile id="3">
-  <image width="16" height="16" source="BG/stars2.png"/>
+  <image width="16" height="16" source="tiles/stars2.png"/>
  </tile>
  <tile id="4">
-  <image width="16" height="16" source="BG/window_corner.png"/>
+  <image width="16" height="16" source="tiles/window_corner.png"/>
  </tile>
  <tile id="5">
-  <image width="16" height="16" source="BG/window_side.png"/>
+  <image width="16" height="16" source="tiles/window_side.png"/>
  </tile>
  <tile id="6">
-  <image width="16" height="16" source="Characters/enenemy_human_butcher.png"/>
+  <image width="16" height="16" source="characters/ennemy_human_butcher.png"/>
   <objectgroup draworder="index" id="2">
    <object id="1" x="2.78261" y="0.0434783" width="11.1304" height="16"/>
   </objectgroup>
  </tile>
  <tile id="7">
-  <image width="16" height="16" source="Characters/ennemy_butcher.png"/>
+  <image width="16" height="16" source="characters/ennemy_butcher.png"/>
   <objectgroup draworder="index" id="2">
    <object id="1" x="5.95652" y="-0.0434783" width="11.9565" height="16.087"/>
   </objectgroup>
  </tile>
  <tile id="8">
-  <image width="16" height="16" source="Characters/ennemy_shield_butcher.png"/>
+  <image width="16" height="16" source="characters/ennemy_shield_butcher.png"/>
   <objectgroup draworder="index" id="2">
    <object id="1" x="2.86957" y="0.0434783" width="15.0435" height="15.9565"/>
   </objectgroup>
  </tile>
  <tile id="9">
-  <image width="16" height="16" source="Characters/pnj_cow.png"/>
+  <image width="16" height="16" source="characters/pnj_cow.png"/>
   <objectgroup draworder="index" id="2">
    <object id="1" x="-0.0434783" y="3.82609" width="15.9565" height="12.087"/>
   </objectgroup>
  </tile>
  <tile id="10">
-  <image width="16" height="16" source="déco/aprons.png"/>
+  <image width="16" height="16" source="tiles/aprons.png"/>
  </tile>
  <tile id="11">
-  <image width="16" height="16" source="déco/blood1.png"/>
+  <image width="16" height="16" source="tiles/blood1.png"/>
  </tile>
  <tile id="12">
-  <image width="16" height="16" source="déco/blood2.png"/>
+  <image width="16" height="16" source="tiles/blood2.png"/>
  </tile>
  <tile id="13">
-  <image width="16" height="16" source="déco/knifes.png"/>
+  <image width="16" height="16" source="tiles/knifes.png"/>
  </tile>
  <tile id="14">
-  <image width="16" height="16" source="déco/steak.png"/>
+  <image width="16" height="16" source="tiles/steak.png"/>
  </tile>
  <tile id="15">
-  <image width="16" height="16" source="Tiles/cracked_intern_floor_tile.png"/>
+  <image width="16" height="16" source="tiles/cracked_intern_floor_tile.png"/>
  </tile>
  <tile id="17">
-  <image width="16" height="16" source="Tiles/floor_tile.png"/>
+  <image width="16" height="16" source="tiles/floor_tile.png"/>
  </tile>
  <tile id="18">
-  <image width="16" height="16" source="Tiles/intern_floor_tile.png"/>
+  <image width="16" height="16" source="tiles/intern_floor_tile.png"/>
  </tile>
  <tile id="19">
-  <image width="16" height="16" source="Tiles/spikes.png"/>
+  <image width="16" height="16" source="tiles/spikes.png"/>
  </tile>
  <tile id="21">
-  <image width="16" height="16" source="Tiles/moving_platform.png"/>
+  <image width="16" height="16" source="tiles/moving_platform.png"/>
  </tile>
- <tile id="25">
-  <image width="8" height="6" source="bullet.png"/>
- </tile>
+
  <tile id="26">
-  <image width="16" height="16" source="Characters/charac_cow.png"/>
+  <image width="16" height="16" source="characters/charac_cow.png"/>
   <objectgroup draworder="index" id="2">
    <object id="1" x="-0.0869565" y="3.73913" width="14.2174" height="12.3043"/>
   </objectgroup>
@@ -95,6 +93,6 @@
   </objectgroup>
  </tile>
  <tile id="29">
-  <image width="16" height="16" source="Tiles/lava.png"/>
+  <image width="16" height="16" source="tiles/lava.png"/>
  </tile>
 </tileset>
