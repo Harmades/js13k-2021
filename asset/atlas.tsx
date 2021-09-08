b8bbd4d7b6834ca5e2b5e6a4dc55f1347548cd36
ftiguidou <florent.tiguidou@gmail.com>
2021-09-08 19:25:33 +0200

    encoding and new lava

--- asset/atlas.tsx
+++ asset/atlas.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="js13k2021" tilewidth="32" tileheight="16" tilecount="28" columns="0">
+<tileset version="1.5" tiledversion="1.7.2" name="js13k2021" tilewidth="32" tileheight="16" tilecount="29" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="16" height="16" source="tiles/bg_tile.png"/>
@@ -104,6 +104,9 @@
   <image width="16" height="16" source="characters/charac_cowboy.png"/>
  </tile>
  <tile id="44">
-  <image width="8" height="6" source="other/bullet.png"/>
+  <image width="16" height="16" source="other/bullet.png"/>
+ </tile>
+ <tile id="46">
+  <image width="16" height="16" source="tiles/lava2.png"/>
  </tile>
 </tileset>
